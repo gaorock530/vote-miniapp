578cbb9579fa51bcec2121951ef432e234a25660
gaorock530 <gaorock530@hotmail.com>
2020-06-27 23:45:49 +0800

    fix route

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001
 app.disable('x-powered-by')
 
 const publicPath = path.join(__dirname, '..', 'build')
-console.log(publicPath)
+
 app.use(express.static(publicPath))
 
 app.listen(PORT, err => console.log(err || `Web-vote-app-server is running at PORT:${PORT}`))
@@ -20,6 +20,5 @@ app.listen(PORT, err => console.log(err || `Web-vote-app-server is running at PO
 // })
 
 app.get('*', (req, res) => {
-  res.status(404)
-  res.sendFile(__dirname + '/404.html')
+  res.sendFile(publicPath + '/index.html')
 })
